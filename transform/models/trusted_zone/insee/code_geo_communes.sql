4866c4e9269ad564ee7eac87695ea22d4c13fe5c
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2022-11-07 16:09:33 +0100

    Adds trusted_zone_insee transformations

---
+++ transform/models/trusted_zone/insee/code_geo_communes.sql
@@ -0,0 +1,15 @@
+select
+    typecom as "type_commune",
+    cast(reg as integer) as "code_region",
+    dep as "code_departement",
+    com as "code_commune",
+    cast(can as integer) as "code_canton",
+    arr as "code_arrondissement",
+    ctcd as "code_ctcd",
+    cast(tncc as integer) as "type_nom_en_clair",
+    ncc as "nom_en_clair",
+    nccenr as "nom_en_clair_enrichi",
+    libelle,
+    comparent as "code_commune_parente"
+from
+    {{ source('raw_zone_insee', 'commune') }}
---
+++ transform/models/trusted_zone/insee/nomenclature_activites_francaises.sql
@@ -0,0 +1,14 @@
+select
+    code_section,
+    libelle_section,
+    code_division,
+    libelle_division,
+    code_groupe,
+    libelle_groupe,
+    code_classe,
+    libelle_classe,
+    code_sous_classe,
+    libelle_sous_classe
+from
+    {{ source('raw_zone_insee', 'naf2008') }}
+    
