e1a8e9063b812ada8c2d2e1840af480eacd9a275
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2022-11-08 16:20:52 +0100

    Updates company_enriched to avoid duplicates

--- transform/models/refined_zone/enriched/company_enriched.sql
+++ transform/models/refined_zone/enriched/company_enriched.sql
@@ -1,3 +1,17 @@
+with communes as (
+    --Il y a des duplicats dans les codes communes, ici on déduplique et on ajoute les données de la commune parente
+    --en cas d'absence des données pour un commune déléguée. Cela concerne très peu de cas néanmoins.
+    select
+    cgc.code_commune,
+    max(coalesce(cgc.code_departement,cgc2.code_departement)) as code_departement,
+    max(coalesce(cgc.code_region,cgc2.code_region)) as code_region
+    from
+        {{ ref('code_geo_communes') }} cgc
+    left join 
+        {{ ref('code_geo_communes') }} cgc2
+    on cgc.code_commune_parente = cgc2.code_commune
+    group by cgc.code_commune
+)
 SELECT
     c.id,
     c.siret,
@@ -40,7 +54,7 @@ FROM
     LEFT JOIN {{ ref('stock_etablissement') }}
     etabs
     ON C.siret = etabs.siret
-    LEFT JOIN {{ ref('code_geo_communes') }}
+    LEFT JOIN communes
     comm
     ON COALESCE(
         etabs.code_commune_etablissement,
