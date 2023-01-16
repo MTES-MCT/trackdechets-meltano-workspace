2c7fd616632a7f6071b8a3f33eef35b45304ef70
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-01-16 18:44:52 +0100

    Updates tap-zammad to add Groups Stream

---
+++ transform/models/trusted_zone/zammad/groups.sql
@@ -0,0 +1,19 @@
+SELECT
+    id,
+    "name",
+    created_at,
+    created_by_id,
+    updated_at,
+    updated_by_id,
+    active,
+    assignment_timeout,
+    email_address_id,
+    follow_up_assignment,
+    follow_up_possible,
+    note,
+    reopen_time_in_days,
+    shared_drafts,
+    signature_id,
+    user_ids
+FROM
+    {{ source("raw_zone_zammad", "groups") }};
