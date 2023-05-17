880fa2e59cadfad9bc1ff8d31187d07b4f12c993
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-05-17 15:21:31 +0000

    Fixes typo

--- transform/models/refined_zone/stats_publiques/bsff/bsff_packagings_processed_by_week.sql
+++ transform/models/refined_zone/stats_publiques/bsff/bsff_packagings_processed_by_week.sql
@@ -4,4 +4,4 @@
     )
 }}
 
-{{ create_bordereaux_counts("bsff_packaging","operation_date", "packagings_processed", "packagings_quantity_rpcoessed") }}
+{{ create_bordereaux_counts("bsff_packaging","operation_date", "packagings_processed", "packagings_quantity_processed") }}
