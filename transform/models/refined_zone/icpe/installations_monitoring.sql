4d3b454af1de07944546b391a7771f1224995495
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-07-12 10:19:13 +0000

    Adds new icpe model

---
+++ transform/models/refined_zone/icpe/installations_monitoring.sql
@@ -0,0 +1,61 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with installations as (
+select
+    siret,
+    rubrique || coalesce('-' || alinea,'') as rubrique,
+    max(raison_sociale) as raison_sociale,
+    array_agg(distinct code_aiot) as codes_aiot,
+    sum(quantite_totale) as quantite_autorisee
+from
+    {{ ref('installations_rubriques') }}
+where
+    siret is not null
+group by
+    siret, rubrique || coalesce('-' || alinea,'')
+    ),
+wastes as (
+select
+    b.destination_company_siret as siret,
+    extract( year
+from
+    b.processed_at) as "year",
+    b.processing_operation,
+    sum(b.quantity_received) as quantite_traitee
+from
+    {{ ref('bordereaux_enriched') }} b
+where
+    b.destination_company_siret in (
+    select
+        siret
+    from
+        installations)
+    and b.processed_at >= '2022-01-01'
+group by
+    b.destination_company_siret ,
+    extract(year
+from
+    b.processed_at),
+    b.processing_operation  
+),
+wastes_rubriques as (
+select
+    wastes.*,
+    mrco.rubrique
+from
+    wastes
+left join trusted_zone.mapping_rubrique_code_operation mrco on
+    wastes.processing_operation = mrco.code_operation 
+)
+select
+    installations.*,
+    wr."year",
+    wr.quantite_traitee
+from
+    installations
+left join wastes_rubriques wr on
+    installations.siret = wr.siret and installations.rubrique = wr.rubrique
