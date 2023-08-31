ef60a028ae4e692647aaebb50413bad62b2bb73e
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-08-31 09:07:31 +0000

    updates meltano valeurs aberrantes

--- transform/models/refined_zone/analytics/bordereaux_valeurs_aberrantes.sql
+++ transform/models/refined_zone/analytics/bordereaux_valeurs_aberrantes.sql
@@ -36,7 +36,7 @@ with bs_data as (
             "_bs_type" = 'BSFF'
             and quantity_received > 20
         ))
-        and received_at >= '2022-01-01'
+        and received_at >= '2023-01-01'
 ),
 
 recipients_data as (
