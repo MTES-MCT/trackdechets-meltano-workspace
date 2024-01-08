97b912a7addfe41e59a63c1f0624dc78ebf5f86a
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-01-08 13:37:02 +0000

    Adds new model for analytics

---
+++ transform/models/refined_zone/analytics/quantite_traitee_par_departement_2023.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+select
+    destination_departement as code_departement,
+    sum(quantity_received) as quantite_traitee
+from
+    {{ ref('bordereaux_enriched') }} be
+where
+    not is_draft
+    and (processed_at between '2023-01-01' and '2023-12-31')
+    and (waste_code ~* '.*\*$'
+        or coalesce(waste_pop ,
+        false)
+            or coalesce(waste_is_dangerous,
+            false))
+    and processed_at is not null
+group by destination_departement 
+order by sum desc nulls last
