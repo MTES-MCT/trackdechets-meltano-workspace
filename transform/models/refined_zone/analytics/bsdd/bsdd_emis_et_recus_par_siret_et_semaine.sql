9c58a48e9f47a2777e0a551dc916a08812760cd8
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-03-29 16:01:13 +0000

    Adds RNDTS data

--- transform/models/refined_zone/analytics/bsdd/bsdd_emis_et_recus_par_siret_et_semaine.sql
+++ transform/models/refined_zone/analytics/bsdd/bsdd_emis_et_recus_par_siret_et_semaine.sql
@@ -12,34 +12,44 @@ with bsdd as (
         received_at
     from
         {{ ref('bsdd') }}
-    where not is_deleted 
-    and status != 'DRAFT'
-    and (
-        {{ dangerous_waste_filter('bsdd') }}
-    )
-        
+    where
+        not is_deleted
+        and status != 'DRAFT'
+        and (
+            {{ dangerous_waste_filter('bsdd') }}
+        )
+
 )
+
 select
     emitter_company_siret as siret,
-    'emis' as flux,
-    date_trunc('week',
-    sent_at) as semaine,
-    count(id) as nombre_bordereaux
-    from bsdd
+    'emis'                as flux,
+    date_trunc(
+        'week',
+        sent_at
+    )                     as semaine,
+    count(id)             as nombre_bordereaux
+from bsdd
 group by
     emitter_company_siret,
-    date_trunc('week',
-    sent_at)
+    date_trunc(
+        'week',
+        sent_at
+    )
 union all
 select
     recipient_company_siret as siret,
-    'reçus' as flux,
-    date_trunc('week',
-    received_at) as semaine,
-    count(*) as nombre_bordereaux
+    'reçus'                as flux,
+    date_trunc(
+        'week',
+        received_at
+    )                       as semaine,
+    count(*)                as nombre_bordereaux
 from
     bsdd
 group by
     recipient_company_siret,
-    date_trunc('week',
-    received_at)
+    date_trunc(
+        'week',
+        received_at
+    )
---
+++ transform/models/trusted_zone/rndts/dnd_sortants.sql
@@ -0,0 +1,48 @@
+{{
+  config(
+    materialized = 'table',
+    indexes=[
+        {"columns":['id'],"unique":True},
+        {"columns":['created_date']},
+        {"columns":['numero_identification_declarant']},
+        ]
+
+    )
+}}
+
+with source as (
+    select * from {{ source('raw_zone_rndts', 'dnd_sortants') }}
+),
+
+renamed as (
+    select
+        {{ adapter.quote("declarant.numero_identification") }} as numero_identification_declarant,
+        {{ adapter.quote("declarant.raison_sociale") }}        as raison_sociale_declarant,
+        {{ adapter.quote("created_year_utc") }},
+        {{ adapter.quote("id") }},
+        {{ adapter.quote("created_date") }},
+        {{ adapter.quote("last_modified_date") }},
+        {{ adapter.quote("date_expedition") }},
+        {{ adapter.quote("is_dechet_pop") }}                   as dechet_pop,
+        {{ adapter.quote("quantite") }},
+        {{ adapter.quote("unite") }},
+        {{ adapter.quote("code_dechet") }},
+        {{ adapter.quote("denomination_usuelle") }},
+        {{ adapter.quote("status") }}
+    from source
+)
+
+select
+    id::integer,
+    created_date::timestamptz,
+    created_year_utc::smallint,
+    last_modified_date::timestamptz,
+    numero_identification_declarant,
+    raison_sociale_declarant,
+    code_dechet,
+    denomination_usuelle,
+    quantite,
+    unite,
+    date_expedition::date,
+    status
+from renamed
