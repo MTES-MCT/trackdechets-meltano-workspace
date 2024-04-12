5cc10db0c4858a2d73973faf46255cea0d81259e
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-04-12 14:38:30 +0000

    updates tra 14189

--- transform/models/refined_zone/analytics/ad_hoc/tra_14189.sql
+++ transform/models/refined_zone/analytics/ad_hoc/tra_14189.sql
@@ -109,10 +109,6 @@ emetteurs as (
 
 grouped as (
     select
-       coalesce(
-            d.siret,
-            e.siret
-        ) as siret,
         num_bsdd,
         quantite_traitee_bsdd,
         num_bsda,
@@ -130,7 +126,11 @@ grouped as (
         num_bsdasri_envoyes,
         quantite_envoyee_bsdasri,
         num_bsvhu_envoyes,
-        quantite_envoyee_bsvhu
+        quantite_envoyee_bsvhu,
+        coalesce(
+            d.siret,
+            e.siret
+        ) as siret
     from
         destinataires as d
     full outer join emetteurs as e
@@ -140,36 +140,102 @@ grouped as (
 
 select
     g.siret,
-    max(coalesce(c.name, ir.raison_sociale)) as nom_etablissement,
-    max(c.gerep_id) as numero_gerep,
-    max(c.id) is not null                    as inscrit_td,
-    max(ir.code_aiot) is not null            as inscrit_gun,
-    array_to_string(array_agg(distinct ir.code_aiot), ', ')         as codes_installations,
-    array_to_string(array_agg(distinct ir.rubrique),', ')          as codes_rubriques,
-    max(num_bsdd)                            as num_bsdd_traites,
-    max(quantite_traitee_bsdd)               as quantite_traitee_bsdd,
-    max(num_bsda)                            as num_bsda_traites,
-    max(quantite_traitee_bsda)               as quantite_traitee_bsda,
-    max(num_bsff)                            as num_bsff_traites,
-    max(quantite_traitee_bsff)               as quantite_traitee_bsff,
-    max(num_bsdasri)                         as num_bsdasri_traites,
-    max(quantite_traitee_bsdasri)            as quantite_traitee_bsdasri,
-    max(num_bsdd_envoyes) as num_bsdd_envoyes,
-    max(quantite_envoyee_bsdd) as quantite_envoyee_bsdd,
-    max(num_bsda_envoyes) as num_bsda_envoyes,
-    max(quantite_envoyee_bsda) as quantite_envoyee_bsda,
-    max(num_bsff_envoyes) as num_bsff_envoyes,
-    max(quantite_envoyee_bsff) as quantite_envoyee_bsff,
-    max(num_bsdasri_envoyes) as num_bsdasri_envoyes,
-    max(quantite_envoyee_bsdasri) as quantite_envoyee_bsdasri,
-    max(num_bsvhu_envoyes) as num_bsvhu_envoyes,
-    max(quantite_envoyee_bsvhu) as quantite_envoyee_bsvhu
+    max(
+        coalesce(
+            c.name,
+            ir.raison_sociale,
+            coalesce(
+                se.enseigne_1_etablissement,
+                se.enseigne_2_etablissement,
+                se.enseigne_3_etablissement
+            )
+        )
+    )             as nom_etablissement,
+    max(
+        coalesce(
+            c.address,
+            coalesce(numero_voie_etablissement, '')
+            || coalesce(type_voie_etablissement, '')
+            || coalesce(libelle_voie_etablissement, '')
+            || coalesce(code_postal_etablissement, '')
+            || coalesce(libelle_commune_etablissement, '')
+        )
+    )             as adresse_etablissement,
+    max(
+        c.gerep_id
+    )             as numero_gerep,
+    max(
+        c.id
+    ) is not null as inscrit_td,
+    max(
+        ir.code_aiot
+    ) is not null as inscrit_gun,
+    array_to_string(
+        array_agg(distinct ir.code_aiot), ', '
+    )             as codes_installations,
+    array_to_string(
+        array_agg(distinct ir.rubrique), ', '
+    )             as codes_rubriques,
+    max(
+        num_bsdd
+    )             as num_bsdd_traites,
+    max(
+        quantite_traitee_bsdd
+    )             as quantite_traitee_bsdd,
+    max(
+        num_bsda
+    )             as num_bsda_traites,
+    max(
+        quantite_traitee_bsda
+    )             as quantite_traitee_bsda,
+    max(
+        num_bsff
+    )             as num_bsff_traites,
+    max(
+        quantite_traitee_bsff
+    )             as quantite_traitee_bsff,
+    max(
+        num_bsdasri
+    )             as num_bsdasri_traites,
+    max(
+        quantite_traitee_bsdasri
+    )             as quantite_traitee_bsdasri,
+    max(
+        num_bsdd_envoyes
+    )             as num_bsdd_envoyes,
+    max(
+        quantite_envoyee_bsdd
+    )             as quantite_envoyee_bsdd,
+    max(
+        num_bsda_envoyes
+    )             as num_bsda_envoyes,
+    max(
+        quantite_envoyee_bsda
+    )             as quantite_envoyee_bsda,
+    max(
+        num_bsff_envoyes
+    )             as num_bsff_envoyes,
+    max(
+        quantite_envoyee_bsff
+    )             as quantite_envoyee_bsff,
+    max(
+        num_bsdasri_envoyes
+    )             as num_bsdasri_envoyes,
+    max(
+        quantite_envoyee_bsdasri
+    )             as quantite_envoyee_bsdasri,
+    max(
+        num_bsvhu_envoyes
+    )             as num_bsvhu_envoyes,
+    max(
+        quantite_envoyee_bsvhu
+    )             as quantite_envoyee_bsvhu
 from
     grouped as g
 left join {{ ref('company') }} as c on g.siret = c.siret
 left join {{ ref('installations_rubriques_2024') }} as ir
     on
         g.siret = ir.siret
-left join {{ ref('stock_etablissement') }} se on g.siret = se.siret
+left join {{ ref('stock_etablissement') }} as se on g.siret = se.siret
 group by
     g.siret
