ef4e39b50f2fe39a22df671b90856763045da56f
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-05-27 09:14:19 +0000

    Adds packagings weight to bsff_enriched
    Also updates installations_daily_processed_wastes
    due to new GUN data extraction.
    
    Finally adds two new models based on RNDTS data.

--- transform/models/refined_zone/enriched/bsff_enriched.sql
+++ transform/models/refined_zone/enriched/bsff_enriched.sql
@@ -19,4 +19,20 @@
     ]
 ) }}
 
-{{ create_bordereaux_enriched_query('bsff',False) }}
+with bsff_data as (
+    {{ create_bordereaux_enriched_query('bsff',False) }}
+),
+
+packagings as (
+    select
+        bsff_id,
+        sum(acceptation_weight) as accepted_quantity_packagings
+    from {{ ref('bsff_packaging') }}
+    group by bsff_id
+)
+
+select
+    bsff_data.*,
+    packagings.accepted_quantity_packagings
+from bsff_data
+left join packagings on bsff_data.id = packagings.bsff_id
---
+++ transform/models/refined_zone/stats_publiques/stats_hebdo/dnd/dnd_quantites_traites_hebdo_par_code_traitement.sql
@@ -0,0 +1,34 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+select
+    date_trunc(
+        'week',
+        date_reception
+    ) as semaine,
+    code_traitement as code_operation,
+    case
+            when code_traitement like 'R%' then 'Déchet valorisé'
+            when code_traitement like 'D%' then 'Déchet éliminé'
+            else 'Autre'
+        end                  as type_operation,
+    sum(
+        case
+            when quantite > 60
+            then quantite / 1000
+            else quantite
+            end
+    ) filter (where unite = 'T') as quantite_traitee,
+    sum(
+        case
+            when quantite > 60
+            then quantite / 1000
+            else quantite
+            end
+    ) filter (where unite='M3') as volume_traite
+from {{ ref('dnd_entrants') }}
+group by 1,2
+order by 1 desc
