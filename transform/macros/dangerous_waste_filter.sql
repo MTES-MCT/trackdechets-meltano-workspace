79c0543e4455ac26e7713b041d5ad8d2194a08eb
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-06-03 15:44:37 +0000

    Adds new departemental model for stats publiques

--- transform/macros/dangerous_waste_filter.sql
+++ transform/macros/dangerous_waste_filter.sql
@@ -3,14 +3,18 @@
     ) -%}
 
     {% if model_name == "bsdd" %}
-            waste_details_code ~* '.*\*$'
+            (waste_details_code ~* '.*\*$'
             OR waste_details_pop
-            or waste_details_is_dangerous  
+            OR waste_details_is_dangerous )
+    {% elif model_name == "bordereaux_enriched" %}
+            (waste_code ~* '.*\*$'
+            OR coalesce(waste_pop,false)
+            OR coalesce(waste_is_dangerous,false))
     {% elif model_name == "bsda" %}
-        waste_code ~* '.*\*$'
-        OR waste_pop
+        (waste_code ~* '.*\*$'
+        OR waste_pop)
     {% else %}
-        waste_code ~* '.*\*$'
+        (waste_code ~* '.*\*$')
     {% endif %}
 
 {%- endmacro %}
