fda22f0280f5fd6f8f74f06d254f06b3c3f61a00
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-06-04 08:48:21 +0000

    Adds new departemental models

--- transform/models/refined_zone/stats_publiques/departementales/weekly_quantity_processed_flows.sql
+++ transform/models/refined_zone/stats_publiques/departementales/weekly_quantity_processed_flows.sql
@@ -1,22 +1,52 @@
 {{
   config(
     materialized = 'table',
+    indexes = [{"columns":["departement_origine","departement_destination","semaine"],"unique":true}]
     )
 }}
 
+with preprocessed_data as (
+    select
+        emitter_departement     as departement_origine,
+        destination_departement as departement_destination,
+        date_trunc(
+            'week', be.processed_at
+        )                       as semaine,
+        coalesce(
+            be.quantity_received, be.accepted_quantity_packagings
+        )                       as quantite_traitee
+    from {{ ref('bordereaux_enriched') }} as be
+    where {{ dangerous_waste_filter('bordereaux_enriched') }}
+    /* Pas de bouillons */
+    and status != 'DRAFT'
+    /* Uniquement codes opérations finales */
+    and processing_operation not in (
+        'D9',
+        'D13',
+        'D14',
+        'D15',
+        'R12',
+        'R13'
+    )
+    /* Uniquement les données jusqu'à la dernière semaine complète */
+    and processed_at < date_trunc(
+        'week',
+        now()
+    )
+    and emitter_departement is not null
+    and destination_departement is not null
+    and quantity_received is not null
+)
+
 select
-  emitter_departement as departement_origine, 
-  destination_departement as departement_destination,
-  date_trunc('week', be.processed_at) as semaine,
-  sum(
-    case 
-      when be.quantity_received > 60 then quantity_received/1000
-      else quantity_received end 
-  ) as quantite_traitee
-from {{ ref('bordereaux_enriched') }} be
-where {{ dangerous_waste_filter('bordereaux_enriched')}}
-and date_trunc('week', be.processed_at) >= '2020-01-01'
-and emitter_departement is not null
-and destination_departement is not null
-and quantity_received is not null
-group by 1,2,3
+    departement_origine,
+    departement_destination,
+    semaine,
+    sum(
+        case
+            when quantite_traitee > 60 then quantite_traitee / 1000
+            else quantite_traitee
+        end
+    ) as quantite_traitee
+from preprocessed_data
+group by 1, 2, 3
