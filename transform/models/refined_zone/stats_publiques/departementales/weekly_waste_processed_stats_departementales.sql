5fca5f94ee47d442a1229f2e880b993cea184275
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-06-04 08:48:21 +0000

    Adds new departemental models

---
+++ transform/models/refined_zone/stats_publiques/departementales/weekly_waste_processed_stats_departementales.sql
@@ -0,0 +1,60 @@
+{{
+  config(
+    materialized = 'table',
+    indexes = [{"columns":["departement","code_operation","semaine"],"unique":true}]
+    )
+}}
+
+with preprocessed_data as (
+    select
+        destination_departement as departement,
+        be.processing_operation as code_operation,
+        date_trunc(
+            'week', be.processed_at
+        )                       as semaine,
+        case
+            when processing_operation like 'R%' then 'Déchet valorisé'
+            when processing_operation like 'D%' then 'Déchet éliminé'
+            else 'Autre'
+        end                     as type_operation,
+        coalesce(
+            be.quantity_received, be.accepted_quantity_packagings
+        )                       as quantite_traitee
+    from {{ ref('bordereaux_enriched') }} as be
+    where
+        /* Uniquement les déchets dangereux */
+        {{ dangerous_waste_filter('bordereaux_enriched') }}
+        /* Pas de bouillons */
+        and status != 'DRAFT'
+        /* Uniquement codes opérations finales */
+        and processing_operation not in (
+            'D9',
+            'D13',
+            'D14',
+            'D15',
+            'R12',
+            'R13'
+        )
+        /* Uniquement les données jusqu'à la dernière semaine complète */
+        and processed_at < date_trunc(
+            'week',
+            now()
+        )
+        and emitter_departement is not null
+        and destination_departement is not null
+        and quantity_received is not null
+)
+
+select
+    departement,
+    code_operation,
+    semaine,
+    max(type_operation) as type_operation,
+    sum(
+        case
+            when quantite_traitee > 60 then quantite_traitee / 1000
+            else quantite_traitee
+        end
+    )                   as quantite_traitee
+from preprocessed_data
+group by 1, 2, 3
--- transform/models/refined_zone/stats_publiques/stats_hebdo/dnd/dnd_quantites_traites_hebdo_par_code_traitement.sql
+++ transform/models/refined_zone/stats_publiques/stats_hebdo/dnd/dnd_quantites_traites_hebdo_par_code_traitement.sql
@@ -8,27 +8,27 @@ select
     date_trunc(
         'week',
         date_reception
-    ) as semaine,
-    code_traitement as code_operation,
+    )                             as semaine,
+    code_traitement               as code_operation,
     case
-            when code_traitement like 'R%' then 'Déchet valorisé'
-            when code_traitement like 'D%' then 'Déchet éliminé'
-            else 'Autre'
-        end                  as type_operation,
+        when code_traitement like 'R%' then 'Déchet valorisé'
+        when code_traitement like 'D%' then 'Déchet éliminé'
+        else 'Autre'
+    end                           as type_operation,
     sum(
         case
             when quantite > 60
-            then quantite / 1000
+                then quantite / 1000
             else quantite
-            end
-    ) filter (where unite = 'T') as quantite_traitee,
+        end
+    ) filter (where unite = 'T')  as quantite_traitee,
     sum(
         case
             when quantite > 60
-            then quantite / 1000
+                then quantite / 1000
             else quantite
-            end
-    ) filter (where unite='M3') as volume_traite
+        end
+    ) filter (where unite = 'M3') as volume_traite
 from {{ ref('dnd_entrants') }}
-group by 1,2
+group by 1, 2
 order by 1 desc
