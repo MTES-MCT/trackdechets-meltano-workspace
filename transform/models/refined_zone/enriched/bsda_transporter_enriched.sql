c975446ee5ec6afe557d17696aea2dbe1f2ba681
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-08-11 17:48:03 +0000

    Adds worker_company_siret to bsda_transporter_enriched

--- transform/models/refined_zone/enriched/bsda_transporter_enriched.sql
+++ transform/models/refined_zone/enriched/bsda_transporter_enriched.sql
@@ -20,6 +20,7 @@ select
     b.created_at as bordereau_created_at,
     b.emitter_company_siret,
     b.destination_company_siret,
+    b.worker_company_siret,
     b.eco_organisme_siret,
     b.waste_code,
     b.destination_reception_weight
