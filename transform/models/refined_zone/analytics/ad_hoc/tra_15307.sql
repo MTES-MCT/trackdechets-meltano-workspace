1bebbd7a3a7cec990f633d9abf3ae01f3c3ff65b
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-10-18 09:42:26 +0000

    Linting

--- transform/models/refined_zone/analytics/ad_hoc/tra_15307.sql
+++ transform/models/refined_zone/analytics/ad_hoc/tra_15307.sql
@@ -9,22 +9,29 @@
 }}
 
 with companies as (
-select 
-	distinct recipient_company_siret as siret
-from
-    {{ ref('bsdd') }}
-where
-	quantity_received = 0
-	and received_at is not null
-	and created_at >= '2023-01-01'
-	and not is_deleted
-	and waste_acceptation_status = 'ACCEPTED'
-	and status in ('RECEIVED', 'FOLLOWED_WITH_PNTTD', 'PROCESSED', 'AWAITING_GROUP', 'NO_TRACEABILITY', 'GROUPED')
+    select distinct recipient_company_siret as siret
+    from
+        {{ ref('bsdd') }}
+    where
+        quantity_received = 0
+        and received_at is not null
+        and created_at >= '2023-01-01'
+        and not is_deleted
+        and waste_acceptation_status = 'ACCEPTED'
+        and status in (
+            'RECEIVED',
+            'FOLLOWED_WITH_PNTTD',
+            'PROCESSED',
+            'AWAITING_GROUP',
+            'NO_TRACEABILITY',
+            'GROUPED'
+        )
 )
-select 
-	c.siret,
-	ca.company_name as nom_etablissement,
-	ca.user_email as email_admin,
-	ca.user_name as nom_admin
-from companies c
-inner join {{ ref('companies_admins') }} ca  on c.siret=ca.company_siret
+
+select
+    c.siret,
+    ca.company_name as nom_etablissement,
+    ca.user_email   as email_admin,
+    ca.user_name    as nom_admin
+from companies as c
+inner join {{ ref('companies_admins') }} as ca on c.siret = ca.company_siret
